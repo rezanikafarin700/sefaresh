0a1b923ee25cd259b6841d303f27da7e16fceadf
rezanikafarin <rezanikafarin100@gmail.com>
2023-04-18 01:28:28 +0430

    create routes

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,33 @@
 <?php
 
+
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+
+
+    Route::post('/login','AuthController@login');
+    Route::post('/logout','AuthController@logout')->middleware('check-token');
+    Route::get('/user','AuthController@user');
+
+    Route::get('/test',function (Request $request){
+    dd('salam reza');
+});
+
+Route::group(['prefix' => 'users'],function (){
+        Route::get('/','UserController@index');
+        Route::get('/{id}','UserController@show');
+        Route::post('/','UserController@store');
+        Route::put('/{id}','UserController@update');
+        Route::delete('/{id}','UserController@destroy');
+});
+
+
+Route::group(['prefix' => 'products'],function (){
+        Route::get('/','ProductController@index');
+        Route::get('/{id}','ProductController@show');
+        Route::post('/','ProductController@store');
+        Route::put('/{id}','ProductController@update');
+        Route::delete('/{id}','ProductController@destroy');
 });
